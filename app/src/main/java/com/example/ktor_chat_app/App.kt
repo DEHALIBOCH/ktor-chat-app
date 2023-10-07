bf4f7f78c885a48e37d6b3be947f78d6c4c06a9d
marat <almabekov.marat@gmail.com>
2023-10-08 02:11:29 +0600

    Application class

---
+++ app/src/main/java/com/example/ktor_chat_app/App.kt
@@ -0,0 +1,20 @@
+package com.example.ktor_chat_app
+
+import android.app.Application
+import com.example.ktor_chat_app.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
