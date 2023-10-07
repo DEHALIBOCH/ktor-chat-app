b7b1d957aecc54318ef78ebf139d2e00ffb16604
marat <almabekov.marat@gmail.com>
2023-10-08 03:01:06 +0600

    DI

--- app/src/main/java/com/example/ktor_chat_app/di/Modules.kt
+++ app/src/main/java/com/example/ktor_chat_app/di/Modules.kt
@@ -1,5 +1,9 @@
 package com.example.ktor_chat_app.di
 
+import com.example.ktor_chat_app.data.ChatSocketService
+import com.example.ktor_chat_app.data.ChatSocketServiceImpl
+import com.example.ktor_chat_app.data.MessageService
+import com.example.ktor_chat_app.data.MessageServiceImpl
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -9,6 +13,7 @@ import io.ktor.serialization.kotlinx.json.json
 import org.koin.dsl.module
 
 val appModule = module {
+
     single<HttpClient> {
         HttpClient(CIO) {
             install(Logging)
@@ -18,4 +23,13 @@ val appModule = module {
             }
         }
     }
+
+    single<MessageService> {
+        MessageServiceImpl(get())
+    }
+
+    single<ChatSocketService> {
+        ChatSocketServiceImpl(get())
+    }
+
 }
