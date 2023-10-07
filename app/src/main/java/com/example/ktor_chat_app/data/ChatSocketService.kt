05f36977b93b63794d3a654bf6a49e71878ec0c8
marat <almabekov.marat@gmail.com>
2023-10-08 02:03:43 +0600

    Base url and endpoint to socket

--- app/src/main/java/com/example/ktor_chat_app/data/ChatSocketService.kt
+++ app/src/main/java/com/example/ktor_chat_app/data/ChatSocketService.kt
@@ -13,4 +13,13 @@ interface ChatSocketService {
     fun observeMessages() : Flow<Message>
 
     suspend fun closeSession()
+
+    companion object {
+        const val BASE_URL = "ws://10.0.2.2:8082"
+    }
+
+    sealed class Endpoints(val url : String) {
+
+        data object ChatSocket : Endpoints("$BASE_URL/chat-socket")
+    }
 }
